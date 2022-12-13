0672c130c7c66c5c539f21422521e756a2522eb2
Mehul Verma <114098918+mehulverma26@users.noreply.github.com>
2022-12-13 11:35:14 +0530

    second commit at 13th dec'22 at 11:35am

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"))
 })
 
- 
-
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
   console.log(req.files)
   let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
